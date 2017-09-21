f35dbf01e5066c92a4c3c556b541fd5b5c007e02
Michel Grootjans <michel.grootjans@persgroep.be>
2017-09-21 21:49:27 +0200

    big refactor

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,112 +1,126 @@
 class Addition
-  def self.can_perform?(operation)
-    operation[0] == "+"
+  def self.can_perform?(description)
+    description[0] == "+"
   end
 
-  def initialize(term)
-    @term = term.to_i
+  attr_reader :description, :term
+  def initialize(description)
+    @description = description
+    @term = description.to_i
   end
 
   def apply(other_term)
-    @term + other_term
-  end
-
-  def render
-    "+" + @term.to_s
+    term + other_term
   end
 end
 
 class Subtraction
-  def self.can_perform?(operation)
-    operation[0] == "-"
+  def self.can_perform?(description)
+    description[0] == "-"
   end
 
-  def initialize(operation)
-    @term = operation.to_i
+  attr_reader :description, :term
+  def initialize(description)
+    @description = description
+    @term = description.to_i
   end
 
   def apply(other_term)
-    other_term + @term
-  end
-
-  def render
-    @term.to_s
+    term + other_term
   end
 end
 
 class Multiplication
-  def self.can_perform?(operation)
-    operation[0] == "x"
+  def self.can_perform?(description)
+    description[0] == "x"
   end
 
-  def initialize(operation)
-    @term = operation[1].to_i
+  attr_reader :description, :factor
+  def initialize(description)
+    @description = description
+    @factor = description[1..-1].to_i
   end
 
-  def apply(other_term)
-    other_term * @term
-  end
-
-  def render
-    "x" + @term.to_s
+  def apply(other_factor)
+    factor * other_factor
   end
 end
 
 class Division
-  def self.can_perform?(operation)
-    operation[0] == "/"
+  def self.can_perform?(description)
+    description[0] == "/"
   end
 
-  def initialize(operation)
-    @term = operation[1].to_i
+  attr_reader :description, :divisor
+  def initialize(description)
+    @description = description
+    @divisor = description[1..-1].to_i
   end
 
-  def apply(other_term)
-    div, mod = other_term.divmod(@term)
+  def apply(dividend)
+    div, mod = dividend.divmod(divisor)
     raise "Error" if mod != 0
     div
-  end
-
-  def render
-    "/" + @term.to_s
   end
 end
 
 class Shift
-  def self.can_perform?(operation)
-    operation == "<<"
+  def self.can_perform?(description)
+    description == "<<"
   end
 
-  def initialize(operation)
+  def initialize(description)
     # no need to save anything, operation contains exactly "<<"
   end
 
   def apply(term)
-    term / 10 # surprised?
-    # 123/10 = 12 in ruby
+    term / 10
+    # surprised? In ruby, these are the rules FOR INTEGERS:
+    # 129/10 = 12
+    # 130/10 = 13
   end
 
-  def render
+  def description
     "<<"
   end
 end
 
 class Replace
-  def self.can_perform?(operation)
-    operation[1..2] == "=>"
+  def self.can_perform?(description)
+    description.include? "=>"
   end
 
-  def initialize(operation)
-    @from = operation[0]
-    @to = operation[3]
+  attr_reader :description, :from, :to
+  def initialize(description)
+    @description = description
+    @from = description.split("=>")[0]
+    @to = description.split("=>")[1]
   end
 
   def apply(number)
-    number.to_s.gsub(@from, @to).to_i
+    number.to_s.gsub(from, to).to_i
+  end
+end
+
+# monkey patching String
+class String
+  def is_i?
+     /\A[-+]?\d+\z/ === self
+  end
+end
+
+class Number
+  def self.can_perform?(description)
+    description.is_i?
   end
 
-  def render
-    @from.to_s + "=>" + @to.to_s
+  attr_reader :description
+  def initialize(description)
+    @description = description
+  end
+
+  def apply(number)
+    (number.to_s + description).to_i
   end
 end
 
@@ -126,29 +140,29 @@ class Calculator
         outcome = operations.inject(start){|current_value, operation| operation.apply(current_value)}
       rescue
         # an illegal operation occurred
+        # just go to the next set of operations
       end
       if outcome == goal
-        solution = operations.map(&:render)
+        solution = operations.map(&:description)
         break        
       end
-  end
+    end
 
     return solution
   end
 
-  
-  @operations = [
-    Addition, Subtraction, Multiplication, Division, Shift, Replace
-  ]
-
   def self.create_operation(operation_description)
-    @operations.each do |operation|
+    operations.each do |operation|
       if operation.can_perform?(operation_description)
         return operation.new(operation_description)
       end
     end
 
     raise "Operation not supported"
+  end
+
+  def self.operations
+    [ Addition, Subtraction, Multiplication, Division, Shift, Replace, Number ]
   end
 end
 
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -11,24 +11,53 @@ describe "calculator" do
   it { expect(Calculator.solve({start: 1,  goal: 2,  moves: 1, operations: ["x2"]})).to eq ["x2"] }
   it { expect(Calculator.solve({start: 12, goal: 1,  moves: 1, operations: ["<<"]})).to eq ["<<"] }
   it { expect(Calculator.solve({start: 12, goal: 13, moves: 1, operations: ["2=>3"]})).to eq ["2=>3"] }
-  
+  it { expect(Calculator.solve({start: 1,  goal: 11, moves: 1, operations: ["1"]})).to eq ["1"] }
+
+  # multiple moves
   it { expect(Calculator.solve({start: 0,  goal: 2,  moves: 2, operations: ["+1"]})).to eq ["+1","+1"] }
   it { expect(Calculator.solve({start: 0,  goal: 1,  moves: 1, operations: ["+1", "-1"]})).to eq ["+1"] }
   it { expect(Calculator.solve({start: 0,  goal: 1,  moves: 1, operations: ["-1", "+1"]})).to eq ["+1"] }
+  it { expect(Calculator.solve({start: 0,  goal: 11, moves: 2, operations: ["1"]})).to eq ["1", "1"] }
+
+  # challenges
   it { expect(Calculator.solve({start: 11, goal: 29, moves: 5, operations: ["/2", "+3", "1=>2", "2=>9"]})).to eq ["+3", "1=>2", "/2", "2=>9", "1=>2"] }
 end
 
-# special operations that require attention
-describe "division" do
+describe Addition do
+  it { expect(Addition.new("+2").apply(2)).to eq 4 }
+  it { expect(Addition.new("+20").apply(2)).to eq 22 }
+end
+
+describe Subtraction do
+  it { expect(Subtraction.new("-2").apply(4)).to eq 2 }
+  it { expect(Subtraction.new("-2").apply(2)).to eq 0 }
+  it { expect(Subtraction.new("-2").apply(0)).to eq -2 }
+  it { expect(Subtraction.new("-20").apply(2)).to eq -18 }
+end
+
+describe Multiplication do
+  it { expect(Multiplication.new("x2").apply(2)).to eq 4 }
+  it { expect(Multiplication.new("x20").apply(2)).to eq 40 }
+end
+
+describe Division do
   it { expect(Division.new("/2").apply(4)).to eq 2 }
+  it { expect(Division.new("/20").apply(40)).to eq 2 }
   it { expect{Division.new("/2").apply(3)}.to raise_error("Error") }
   it { expect{Division.new("/0").apply(2)}.to raise_error(ZeroDivisionError) }
 end
 
-describe "shift" do
+describe Shift do
   it { expect(Shift.new("<<").apply(1234)).to eq 123 }
 end
 
-describe "replace" do
+describe Replace do
   it { expect(Replace.new("2=>3").apply(12)).to eq 13 }
+  it { expect(Replace.new("23=>45").apply(1236)).to eq 1456 }
+end
+
+describe Number do
+  it { expect(Number.new("1").apply(0)).to eq 1 }
+  it { expect(Number.new("1").apply(1)).to eq 11 }
+  it { expect(Number.new("4").apply(123)).to eq 1234 }
 end
